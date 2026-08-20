bff82fe3f03a0f1605480ee176ed6e9b574269e3
User <user@Users-MacBook-Pro-2.local>
2026-08-20 15:18:35 +0200

    Edit room route and schema feature added

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
-from sqlalchemy import select
 from fastapi import FastAPI
-from app.database.database import SessionLocal
-from app.models.room import Room
+
+from app.database.database import Base, engine
+from app.routes.edit_room import router
 from app.routers.list_rooms import list_router
 from app.routers.list_by_capacity import list_by_capacity_router
 
+app = FastAPI()
 
-with SessionLocal() as session:
-    stmt = select(Room)
-    room = session.scalars(stmt)
-    
-app = FastAPI()  
-
-
-
+app.include_router(router)
 app.include_router(list_router)
 app.include_router(list_by_capacity_router)
+
+Base.metadata.create_all(bind=engine)
