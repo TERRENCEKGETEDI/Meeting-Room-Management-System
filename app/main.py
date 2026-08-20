5684c79ea17b9c7c036d86ec6e889fc11e5c1b12
user <user@users-MacBook-Pro.local>
2026-08-20 16:17:35 +0200

    Updated add room

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import select
-
-from app.database.database import SessionLocal
-from .models.room import Room
-
-
-with SessionLocal() as session:
-    stmt = select(Room)
-    room = session.scalars(stmt)
