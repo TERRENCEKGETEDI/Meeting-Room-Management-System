5588791fcd16d4e9bdd33a1fb268058097cc08a1
Terrence <user@Loaner-MacBook-Pro.local>
2026-08-20 11:46:21 +0200

    room model

--- app/models/room.py
+++ app/models/room.py
@@ -1,8 +1,8 @@
 """SQLAlchemy model for Room"""
 
 from app.database.database import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Boolean, Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
 
 
 class Room(Base):
@@ -10,44 +10,22 @@ class Room(Base):
     Represent Rooms stored in the database
 
     Attributes:
-        room_id: Primary Key
-        user_id: the user who created the id
-        room_name: the name of the room
+        id: Primary Key
+        name: the name of the room
         floor: the floor which the room is
-        room_capacity: max number of staff the room can hold
-        is_available: confirms if the book is booked/not available or not
-        location: where the building is located
-
-        user: a list of rooms can belong to a user (Many-to-One)
-        location: a list of rooms can belong to a location (Many-to-One)
+        capacity: max number of staff the room can hold
     """
     __tablename__ = "room"
-    room_id: Mapped[int] = mapped_column(
+    id: Mapped[int] = mapped_column(
         Integer,
         primary_key=True
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.user_id")
-    )
-    room_name: Mapped[str] = mapped_column(
+    name: Mapped[str] = mapped_column(
         String
     )
     floor: Mapped[str] = mapped_column(
         String
     )
-    room_capacity: Mapped[int] = mapped_column(
+    capacity: Mapped[int] = mapped_column(
         Integer
-    )
-    is_available: Mapped[bool] = mapped_column(
-        Boolean
-    )
-    location_id: Mapped[int] = mapped_column(
-        ForeignKey("location.location_id")
-    )
-
-    user: Mapped["Users"] = relationship(
-        back_populates="rooms"
-    )
-    location: Mapped["Location"] = relationship(
-        back_populates="rooms"
     )
