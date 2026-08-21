c06730a01e5bfc8f4e26b5f0e01d6297a64df743
Bongiwe Magagule <believebongiwe@gmail.com>
2026-08-21 09:15:11 +0200

    Updated list routes

--- app/routes/list_rooms.py
+++ app/routes/list_rooms.py
@@ -3,18 +3,18 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.models.room import Room
-from app.database.database import get_db
+from app.database.database import SessionLocal
 from app.schemas.room import RoomResponse
 
 
 list_router = APIRouter(
-    prefix="/rooms",
-    tags=["rooms"]
+    prefix="/list",
+    tags=["List"]
 )
 
 
 @list_router.get("/", response_model=list[RoomResponse])
-def list_all_rooms(db: Session = Depends(get_db)):
+def list_all_rooms():
     """
     Get a list of all rooms.
 
@@ -24,9 +24,9 @@ def list_all_rooms(db: Session = Depends(get_db)):
     Returns:
         A list of all rooms.
     """
+    with SessionLocal() as session:
+        stmt = select(Room)
+        result = session.execute(stmt)
+        rooms = result.scalars().all()
 
-    stmt = select(Room)
-    result = db.execute(stmt)
-    rooms = result.scalars().all()
-
-    return rooms
+        return rooms
