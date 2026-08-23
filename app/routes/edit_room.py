6f173d61715e59b60e2e725580906b38966f319f
User <user@Users-MacBook-Pro-2.local>
2026-08-23 14:38:24 +0200

    correct exception handling

--- app/routes/edit_room.py
+++ app/routes/edit_room.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
-
+from sqlalchemy.exc import IntegrityError
 from app.database.database import SessionLocal
 from app.models.room import Room
 from app.schemas.room import RoomEdit
@@ -29,14 +29,23 @@ def edit_room(room_id: int,room_edit: RoomEdit):
                status_code=404,
                detail="The room id does not exist"
            )
+        
         if room_edit.name is not None:
             user_result.name = room_edit.name
         if room_edit.capacity is not None:
             user_result.capacity = room_edit.capacity
         if room_edit.floor is not None:
             user_result.floor = room_edit.floor
+        try:
 
-        session.commit()
-        session.refresh(user_result)
+            session.commit()
+            session.refresh(user_result)
+        except IntegrityError:
+            session.rollback()
+
+            raise HTTPException(
+                status_code=400,
+                detail="Could not update the room"
+            )
 
         return user_result
