86a5dd4d9690974a2605593b9e294e967753ccff
user <user@users-MacBook-Pro.local>
2026-08-24 11:01:34 +0200

    Updated whitespaces and HTTPstatus2

--- app/routes/add_room.py
+++ app/routes/add_room.py
@@ -16,7 +16,7 @@ router = APIRouter(
 @router.post(
     "/",
     response_model=RoomResponse,
-    status_code=status.HTTP_201_CREATED
+    status_code=201
 )
 def add_room(room: RoomCreate):
     """
