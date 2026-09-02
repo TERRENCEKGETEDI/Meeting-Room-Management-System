646b718cd8790b7395553da1e4d4547430d9d32e
Bongiwe Magagule <believebongiwe@gmail.com>
2026-09-02 08:29:45 +0200

    Moved list endpoint to rooms.py

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -1,14 +1,42 @@
 """Delete room route"""
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from app.dependencies.database import get_db
 from app.models.room import Room
+from app.schemas.room import RoomResponse
 
 router = APIRouter(prefix="/rooms", tags=["Rooms"])
+
+
+
+
+@router.get("/", response_model=list[RoomResponse])
+def list_all_rooms(min_capacity: int | None = Query(default=None, gt=0) , session:Session  = Depends(get_db)):
+    """
+    Get a list of all rooms. Optionally filtered by minimum capacity
+
+    Args:
+        session: Database session used to access the database.
+        min_capacity: Optional minimum room capacity
+                      It must be greater than 0
+
+    Returns:
+        A list of all rooms , filtered by minimum capacity if provided
+    """
+
+    stmt = select(Room)
+
+    if min_capacity is not None:
+        stmt = stmt.where(Room.capacity >= min_capacity)
+
+
+    rooms = session.scalars(stmt).all()
+
+    return rooms
 
 
 @router.delete("/{room_id}")
