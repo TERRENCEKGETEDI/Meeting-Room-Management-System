9cc606e17db5fcb05f618e716cbcb4cabdad979e
User <user@Users-MacBook-Pro-2.local>
2026-09-02 08:34:25 +0200

    moved the edit room
    # Conflicts:
    #	app/routes/rooms.py

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -1,42 +1,15 @@
 """Delete room route"""
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from app.dependencies.database import get_db
 from app.models.room import Room
-from app.schemas.room import RoomResponse
+from app.schemas.room import RoomEdit, RoomResponse
 
 router = APIRouter(prefix="/rooms", tags=["Rooms"])
-
-
-@router.get("/", response_model=list[RoomResponse])
-def list_all_rooms(
-    min_capacity: int | None = Query(default=None, gt=0),
-    session: Session = Depends(get_db) # noqa: B008
-):
-    """
-    Get a list of all rooms. Optionally filtered by minimum capacity
-
-    Args:
-        session: Database session used to access the database.
-        min_capacity: Optional minimum room capacity
-                      It must be greater than 0
-
-    Returns:
-        A list of all rooms , filtered by minimum capacity if provided
-    """
-
-    stmt = select(Room)
-
-    if min_capacity is not None:
-        stmt = stmt.where(Room.capacity >= min_capacity)
-
-    rooms = session.scalars(stmt).all()
-
-    return rooms
 
 
 @router.delete("/{room_id}")
@@ -54,7 +27,7 @@ def delete_room(
     Returns:
         message: Room deleted or Room not found if room doesn't exist
     """
-
+    
     stmt = select(Room).where(
         Room.id == room_id
     )
@@ -71,7 +44,96 @@ def delete_room(
         session.rollback()
         raise HTTPException(
             status_code=409,
-            detail="Failed to delete room, room might be linked to other tables, try again",
+            detail="Failed to delete room, room might be linked to other tables, try again"
         )
 
     return {"message": "Room deleted"}
+
+@router.patch("/{room_id}", response_model=RoomResponse)
+def edit_room(room_id: int, 
+              room_edit: RoomEdit,
+              session: Session = Depends(get_db)
+):
+    """
+    Edits the details of a room.
+
+    Arguments:
+        room_id: ID of the room to edit.
+        room_edit: Fields to update.
+
+    Return:
+        returns the rooms details
+    """
+    # If the floor, name, and capacity are not entered than return a HTTPException
+    if (
+        room_edit.floor is None
+        and room_edit.name is None
+        and room_edit.capacity is None
+    ):
+        raise HTTPException(status_code=400, detail="No details provdided")
+
+    # if invalid values were provided
+    if ((room_edit.floor is not None and room_edit.floor.isspace()) or
+            (room_edit.name is not None and room_edit.name.isspace())):
+        raise HTTPException(
+            status_code=400,
+            detail="Floor OR Name cannot contain a blank space"
+        )
+
+    if room_edit.capacity is not None and room_edit.capacity <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail="Capacity is less than or equal to 0"
+        )
+
+    # Open a database session for the duration of the request.
+
+    stmt = select(Room).where(Room.id == room_id)
+    room_result = session.scalars(stmt).first()
+    # Catches a exception in case the room id ,is not found
+    if room_result is None:
+        raise HTTPException(
+            status_code=404,
+            detail="The room id does not exist"
+        )
+    
+    changes_made = False
+
+    # Update the room name only when a new name was provided.
+    if room_edit.name is not None:
+        new_name = room_edit.name.strip()
+        # Checks if changes were made
+        if new_name != room_result.name:
+            room_result.name = new_name
+            changes_made = True
+
+    # Update the room capacity only when a new name was provided.
+    if room_edit.capacity is not None:
+        if room_edit.capacity != room_result.capacity:
+                        room_result.capacity = room_edit.capacity
+                        changes_made = True
+
+    # Update the room floor only when a new name was provided.
+    if room_edit.floor is not None:
+        new_floor = room_edit.floor.strip()
+
+        if new_floor != room_result.floor:
+            room_result.floor = new_floor
+            changes_made = True
+
+    if not changes_made:
+            raise HTTPException(
+                status_code=400,
+                detail="No changes made"
+            )    
+        
+    try:
+        session.commit()
+        session.refresh(room_result)
+    except IntegrityError:  # Catching a NOtNUllViolation/UniqueViolation  to rollback the transaction
+        session.rollback()
+
+        raise HTTPException(status_code=409, detail="This room name already exists")
+
+    return room_result
+
