79b7d9dfdce013f3dcdffc0a1a338a6f115b8b0a
Bongiwe Magagule <believebongiwe@gmail.com>
2026-09-02 11:11:28 +0200

    Added functions to hash and verify passwords

--- app/dependencies/security.py
+++ app/dependencies/security.py
@@ -1,21 +1,45 @@
 
-from fastapi import Depends , HTTPException
+from fastapi import Depends, HTTPException
+from pwdlib import PasswordHash
 
 from app.models.user import User
 
-# Hash password, return hashed password
+password_hash = PasswordHash.recommended()
 
+
+# Hash password, return hashed password
+def hash_password(password: str):
+    """ 
+    Hash a plain-text password 
+
+    Args:
+        password: the plaintext password to hash
+
+    Returns:
+        hashed password
+    """
+    return password_hash.hash(password)
+   
+   
 # verity password, return boolean
+def verify_password(password: str, hashed_password: str):
+    """Verify if the plaintext message matches the hashed passsword  
+
+    Args:
+        password (str): plaintext password
+        hashed_password (str): the hashed password 
+
+    Returns:
+        If the password matches return true else false
+    """
+    return password_hash.verify(password, hashed_password)
 
 # get current user, return user
 
 
 # require admin
-
-
-
 def require_admin(
-     current_user :User = Depends(get_current_user)
+     current_user: User = Depends(get_current_user)
 ):
     """
     Verify that the current user has admin privileges.
