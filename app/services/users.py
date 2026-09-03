696bf07ca1a08dbc3fc733d47de4db1dd3bad9f8
Bongiwe Magagule <believebongiwe@gmail.com>
2026-09-03 08:31:15 +0200

    Create user services

--- app/services/users.py
+++ app/services/users.py
@@ -1,11 +1,14 @@
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from app.auth.jwt import create_access_token
-from app.dependencies.security import verify_password
+from app.dependencies.database import get_db
+from app.dependencies.security import hash_password, verify_password
 from app.models.user import User
+from app.schemas.user import UserCreate
 
 
 def login_services(
@@ -47,3 +50,51 @@ def login_services(
             "token_type": "bearer"
         }
     
+def create_user_service(
+    user: UserCreate,
+    session: Session
+):
+    """
+    Creates a New user
+
+    Args:
+        UserCreate = A pydantic schema used to create a user
+        session = A database session life cycle
+
+    Return:
+        The details of the user
+    """
+    stripped_fullname = user.full_name.strip()
+    stripped_username = user.username.strip()
+    stripped_password = user.password.strip()
+
+    if not stripped_username or not stripped_password or not stripped_fullname:
+        raise HTTPException(
+            status_code=400,
+            detail="Fullname or Username or password cannot be empty"
+        )
+
+    hashed_password = hash_password(stripped_password)
+
+    new_user = User(
+        full_name=stripped_fullname,
+        username=stripped_username,
+        password=hashed_password,
+        role="user"
+    )
+
+    try:
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+
+    except IntegrityError:
+        session.rollback()
+
+        raise HTTPException(
+            status_code=409,
+            detail="The username already exists!"
+        )
+
+    return new_user
+
